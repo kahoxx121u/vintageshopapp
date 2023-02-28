ac35e827c03913fa41ddf7bd0b1715b6323a37b1
kahoxx121u <smileyxoxokaho1212@gmail.com>
2023-02-28 19:21:39 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   validates :item, presence: true
   validates :text, presence: true
-  validates :category_id, presence: true
   validates :price, presence: true
   belongs_to :admin
 end
