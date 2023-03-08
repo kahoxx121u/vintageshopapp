5066c8764b4e08cd1ebb1faa5b5fe9c97e08d476
kahoxx121u <smileyxoxokaho1212@gmail.com>
2023-03-09 00:41:53 +0900

    adminの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,20 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-  resources :items, only: [:show] do
+  resources :items do
     scope module: :items do
       resources :add_to_carts, only: [:create]
       resources :delete_in_carts, only: [:create]
     end
   end
 
-  resource :cart, only: [:show] 
+  resource :cart, only: [:show ]
   
   resource :charge, only: [:create]
 
 
   namespace :admins do
     get 'toppages/index'
-    resources :items, only: [:new, :create]
+    resources :items
   end
 end
